ed2f13f77d916d5c87bb9b7b49b54e984d822089
Thibaut <thibaut.tucoulou@sfr.fr>
2018-04-25 00:49:32 +0200

    Methode detection_obstacle+ ajout des methodes utiles de main2.py

--- geometric_situation.py
+++ geometric_situation.py
@@ -14,6 +14,21 @@ FRAME_WIDTH = 1280
 HALF_HORIZONTAL_FIELD_VIEW = HORIZONTAL_FIELD_VIEW/2
 ANGLE_BY_PIXEL = HORIZONTAL_FIELD_VIEW/FRAME_WIDTH
 
+class objet:
+	def __init__(self,Nom,X,Y,Largeur,Hauteur):
+		self.nom=Nom
+		self.x=X
+		self.y=Y
+		if(Nom=="table"):
+			real_object_height=80
+		elif(Nom=="chaise"):
+			real_object_height=80
+		else:
+			real_object_height=20
+		self.largeur=Largeur
+		self.hauteur=Hauteur
+		self.distance=calculate_distance(real_object_height,Hauteur)
+		self.angle=calculate_angle(X)
 
 #We calculate the distance from the center
 def normalize_X(x):
@@ -46,6 +61,39 @@ def detection_simple(x,distance):
 		return "l'objet se trouve un peu plus loin sur votre gauche"
 	else:
 		return "l'objet se trouve un peu plus loin sur votre droite"
+
+
+def liste_des_objets (string):
+	liste=[]
+	if (string):
+		tokens=string.split(';')[:-1]
+		if(tokens):
+			for case in tokens:
+				tab=case.split(',')
+				new_objet=objet(tab[0],int(tab[1]),int(tab[2]),int(tab[3]),int(tab[4]))
+				liste.append(new_objet)
+	return liste
+
+
+def detection_obstacle(liste,index_in_liste,seuil_angle):   #index_in_liste est l'index dans la liste de l'objet que je souhaite atteindre
+    if (liste):
+        if(len(liste)==1):
+            return "Il n'y a aucun obstacle devant l'objet"
+        else:
+	    angle_objet=calculate_angle(liste[index_in_liste].x)
+            projete_mediane_objet=orthogonal_projection(liste[index_in_liste].angle,liste[index_in_liste].distance)
+            for element in liste:
+		angle_element=calculate_angle(element.x)
+		projete_mediane_obstacle=orthogonal_projection(element.angle,element.distance)
+		if(projete_mediane_obstacle<projete_mediane_objet):
+			if(angle_element+seuil_angle>angle_objet and angle_element<angle_objet):
+                    		return "Obstacle detecte "+element.nom
+                	elif(angle_element-seuil_angle<angle_objet and angle_element> angle_objet):
+                    		return "Obstacle detecte "+element.nom
+            return "Il n'y a aucun obstacle devant l'objet"
+    else:
+        return "Il n'y a aucun objet dans votre champ de vision"
+
 
 
 if __name__ == '__main__':
--- object_situation.py
+++ object_situation.py
@@ -2,5 +2,13 @@ from geometric_situation import *
 
 
 if __name__ == "__main__":
-	print(orthogonal_projection1(calculate_angle(1280), 6))
-	print(detection_simple(492,4))
+	#print(orthogonal_projection1(calculate_angle(1280), 6))
+	#print(detection_simple(1100,1.5))
+	liste=list()
+	table=objet("table",500,500,200,200)
+	chaise=objet("chaise",560,50,200,200)
+	#ordinateur=objet("ordinateur",1000,500,50,100)
+	liste.append(table)
+	liste.append(chaise)
+	#liste.append(ordinateur)
+	print(detection_obstacle(liste,0,30))
